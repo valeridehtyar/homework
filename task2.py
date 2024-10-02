28083ace6829129384af0c1b34de031ed6a54082
Валера <dik1965@yandex.ru>
2024-10-02 20:57:14 +0700

    01/10

---
+++ task2.py
@@ -0,0 +1,9 @@
+print('Variable')# Переменная
+execution_homework=12 # выполненных ДЗ
+print(execution_homework)
+spent_hour=1.5 #затрачено времени
+print(spent_hour)
+name='Python'# название курса
+print(name)
+print(1.5/12)#время выполнения одного ДЗ
+print('End')
