cab6bb7f2062b33a0538ed4e5a596a01a9261944
Валера <dik1965@yandex.ru>
2024-10-04 14:40:11 +0700

    04/10

---
+++ task3.py
@@ -0,0 +1,8 @@
+print('lines and indexing lines')# Строки и индексация строк
+example='Топинамбур'
+print(example[0])#  Вывел первый символ
+print(example[-1])# вывел последний символ
+print(example[5:])# вывел вторую половину
+print(example[::-1])# Вывел слово наоборот
+print(example[1:10:2])# вывел каждый второй символ
+print('End')
