da1164be0ee051c1897d67457a0363a3ec5a3b11
Валера <dik1965@yandex.ru>
2024-10-14 20:13:44 +0700

    13/10

---
+++ task1.py
@@ -0,0 +1,12 @@
+print('Houmwork')
+print('1st program')# Арифметика
+print(9**0,5*5)
+print('2nd program')# Логика
+print(9.99>9.98 and 1000!=1000.1)
+print('3rd program')# Школьная загадка
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+print('4th program')# Первый после точки
+print(int(float('123.456')*10)%10)
+print('End')
