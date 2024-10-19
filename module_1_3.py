42041161261abec7cf14b2b016e5e1ca1238e00c
Валера <dik1965@yandex.ru>
2024-10-19 17:55:47 +0700

    19/10

--- module_1_3.py
+++ module_1_3.py
@@ -1,9 +1,10 @@
 name="Valeri"# создал переменную имени
-print(name,type(name)) # вывел на экран
-age=58 # создал переменную возраста
-print(age,type(age)) # вывел на экран
-new_age=59
+print(name) # вывел на экран
+age=(58) # создал переменную возраста
+print(age) # вывел на экран
+new_age=58+15
+print(new_age)
 print(age>new_age) # перезаписал в age+ добавил новое, неверно
 name='is student' # создал новую переменную
-print((name),'true') # вывел значение переменной
+print((name), True ) # вывел значение переменной
 print('End')
