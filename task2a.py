490abb0c81af71c6209598475cce831dbc45ff3b
Валера <dik1965@yandex.ru>
2024-10-23 21:30:25 +0700

    23/10

---
+++ task2a.py
@@ -0,0 +1,5 @@
+execution_homework = 12
+spent_hour = 1.5
+Cours_name = 'Python'
+Time_of_one_execution_homework = spent_hour / execution_homework
+print(Cours_name,',всего задач: ',execution_homework,',затрачено часов: ',spent_hour,',среднее выполнение: ',Time_of_one_execution_homework,'часа')
