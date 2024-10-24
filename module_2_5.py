a837ef56677352c94b7a1f25a094d87ed515d8bc
Валера <dik1965@yandex.ru>
2024-10-24 20:39:26 +0700

    24/10

---
+++ module_2_5.py
@@ -0,0 +1,15 @@
+def get_matrix(n,m,value) :
+    matrix = []
+    for a in range(0,n) :
+        mat1 = []
+        for b in range(0,m) :
+            mat1.append(value)
+        matrix.append(mat1)
+    return matrix
+
+result1 = get_matrix(2,2,10)
+result2 = get_matrix(3,5,42)
+result3 = get_matrix(4,2,13)
+print(result1)
+print(result2)
+print(result3)
