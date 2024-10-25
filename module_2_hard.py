77b5798955c4efc19e335b8a9f078a70d4cc83d8
Валера <dik1965@yandex.ru>
2024-10-25 22:08:57 +0700

    25/10

---
+++ module_2_hard.py
@@ -0,0 +1,15 @@
+from module_2_5 import result3
+
+n =int(input('Введите целое число от 3 до 20: '))
+def get_password(number) :
+    password = ''
+    for i in  range(1,number) :
+        for j in range(2,number) :
+            if j <= i :
+                continue
+            if number % (i+j) == 0 :
+                password += str(i) + str(j)
+    return password
+
+result = get_password(n)
+print('Пароль', result)
