b55f382a666c45771c207a92a8073c899d56a2b7
Валера <dik1965@yandex.ru>
2024-11-29 20:59:24 +0700

    19/11

--- module_3_2.py
+++ module_3_2.py
@@ -1,6 +1,6 @@
-from pyexpat.errors import messages
 
-def send_email( message, recipient, sender = 'university.help@mail.ru') :
+
+def send_email( message,*,recipient, sender = 'university.help@mail.ru') :
     if '@' not in recipient or not recipient.endswith(('.com','.ru','.net')) or not sender.endswith(('.com','.ru','.net')) :
         print(f'Невозможно отправить письмо с адреса {sender} на адрес { recipient}.')
         
@@ -13,10 +13,10 @@ def send_email( message, recipient, sender = 'university.help@mail.ru') :
     elif sender != 'university.help@mail.ru' :
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
 
-send_email('message','vasyok1337@gmail.com')
+send_email('message',recipient='vasyok1337@gmail.com')
 
-send_email('message','urban.fan@mail.ru','urban.info@gmail.com')
+send_email('message',recipient='urban.fan@mail.ru',sender= 'urban.info@gmail.com')
 
-send_email('message','urban.student@mail.ru', 'urban.teacher@gmail.uk')
+send_email('message',recipient='urban.student@mail.ru',sender= 'urban.teacher@gmail.uk')
 
-send_email('message','university.help@mail.ru', 'university.help@gmail.com')
+send_email('message',recipient='university.help@mail.ru',sender='university.help@gmail.com')
