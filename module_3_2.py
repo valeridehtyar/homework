b4ea1479f0f6a99cc79775896688525526efb0ba
Валера <dik1965@yandex.ru>
2024-12-04 22:00:06 +0700

    04/12

--- module_3_2.py
+++ module_3_2.py
@@ -1,6 +1,6 @@
 
 
-def send_email( message,*,recipient, sender = 'university.help@mail.ru') :
+def send_email( message,recipient,*,sender = 'university.help@mail.ru') :
     if '@' not in recipient or not recipient.endswith(('.com','.ru','.net')) or not sender.endswith(('.com','.ru','.net')) :
         print(f'Невозможно отправить письмо с адреса {sender} на адрес { recipient}.')
         
