93b76cfd6758ad0688db68f59cbd571b7e035b9a
Валера <dik1965@yandex.ru>
2024-12-12 20:35:26 +0700

    12/12

--- module_5_3.py
+++ module_5_3.py
@@ -17,10 +17,13 @@ class House:
         return f'Название: { self.name},кол-во этажей: { self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other. number_of_floors
+        if isinstance(other.number_of_floors,int) and isinstance(other, House):
+            return self.number_of_floors == other. number_of_floors
+
 
     def __it__ (self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+             return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
         return self.number_of_floors <= other.number_of_floors
