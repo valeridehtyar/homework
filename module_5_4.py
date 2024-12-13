e75414f089fea5407352d69a5594dbe51910af51
Валера <dik1965@yandex.ru>
2024-12-13 20:17:13 +0700

    13/12

---
+++ module_5_4.py
@@ -0,0 +1,72 @@
+class House:
+    house_history = []
+
+    def __new__ (cls, *args, **kwargs):
+        cls.house_history.append(args[0])
+        print(*cls.house_history)
+        return super().__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return  self.number_of_floors
+
+    def __str__(self):
+        title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+        return title
+
+    def __eq__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+    def __del__(self):
+        print(self. name, 'снесён, но он останется в истории')
+
+skyscraper = House ('Высотка',20)
+hangar = House ('Ангар',2)
+
+print(skyscraper)
+print(hangar)
+
+del hangar
+print(House.house_history [0],'- первое здание')
+print(House.house_history [-1],'-последнее здание')
+del skyscraper
+print(House.house_history,'-список зданий')
+
+
+
