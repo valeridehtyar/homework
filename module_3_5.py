c81bdf7c847a1660804fed111f0ef2cdf5f9aa7a
Валера <dik1965@yandex.ru>
2024-12-14 14:03:29 +0700

    14/24

--- module_3_5.py
+++ module_3_5.py
@@ -2,12 +2,14 @@
 
 
 def get_multiplied_digits( number) :
-    str_number = str(number)
+    str_number = str(number).rstrip('0')
     if len(str_number) > 1 :
         first = int(str_number[0])
-        return first * get_multiplied_digits(int(str_number[1 :]))
+        return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return int(str_number)
 
 result = get_multiplied_digits(40203)
+result2 = get_multiplied_digits(402030)
 print(result)
+print(result2)
