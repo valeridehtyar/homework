fae149020a59cefbe7071a7a6673e0ccb5b0bcd6
Валера <dik1965@yandex.ru>
2024-12-18 20:57:32 +0700

    18/24

---
+++ module_6_1.py
@@ -0,0 +1,66 @@
+
+
+class Animal:
+    def __init__(self, name):
+        self.alive = True
+        self.fed = False
+        self.name = name
+
+class Plant:
+    def __init__(self, name, edible=False):
+        self.edible = edible
+        self.name = name
+
+class Mammal(Animal):
+    def eat(self, food):
+        if isinstance(food, Plant):
+            if food.edible:
+                 print(f'{self.name} съел {food.name}')
+                 self.fed = True
+            else:
+                print(f'{self.name} не стал есть {food.name}')
+        else:
+            print(f'{food.name} не  растение.{self.name} не  съедобно.')
+
+class Predator(Animal):
+    def eat(self, food):
+        if isinstance(food,Plant):
+            if food.edible:
+                print(f'{self.name} съел {food.name}')
+                self.fed = True
+            else:
+                print(f'{self.name} не стал есть {food.name}')
+                self.alive = False
+        else:
+            print(f' {food.name} не является растением {self.name} не съедобно')
+
+class Flower(Plant):
+    def __init__(self, name):
+        super().__init__(name, False)
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name, True)
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+# Выводим имя хищника
+print(a1.name)
+# Выводим название цветка
+print(p1.name)
+# Выводим живой ли хищник
+print(a1.alive)
+# Выводим сытый ли млекопитающее
+print(a2.fed)
+# Хищник пробует съесть цветок
+a1.eat(p1)
+# Млекопитающее пробует съесть фрукт
+a2.eat(p2)
+# Выводим живой ли хищник после поедания не съедобного растения
+print(a1.alive)
+# Выводим сытое ли млекопитающее после фрукта
+print(a2.fed)
